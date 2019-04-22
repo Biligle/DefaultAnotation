0fffe93b411828c7461539d2e5f920dfde4913d1
wangguoli <wangguoli@bicai365.com>
2019-04-22 15:47:18 +0800

    自定义滑动button

--- app/src/main/java/com/biligle/defaultanotation/CustomView.java
+++ app/src/main/java/com/biligle/defaultanotation/CustomView.java
@@ -3,30 +3,22 @@ package com.biligle.defaultanotation;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 /**
  * @Author wangguoli
  */
-public class CustomView extends View {
+public class CustomView extends AppCompatButton {
 
     private int lastX, lastY;
     private int maxWidth, maxHeight;
-    private Paint wordPaint;
-    private int word_color = Color.RED;//字体颜色
-    private String word_content = "";//
 
     public CustomView(Context context) {
         super(context);
@@ -55,10 +47,6 @@ public class CustomView extends View {
         maxWidth = dm.widthPixels;
         maxHeight = dm.heightPixels - getNavigationBarHeight() - getStatusHeight(context) - getTitleHeight((Activity) context) - getActionBatHeight(context);
         /*---------------------------------------初始化高度结束——————————————————————————————————————*/
-
-        /*---------------------------------------初始化画笔开始——————————————————————————————————————*/
-        wordPaint = new Paint();
-        /*---------------------------------------初始化画笔结束——————————————————————————————————————*/
     }
 
     @Override
@@ -97,13 +85,6 @@ public class CustomView extends View {
                 break;
         }
         return super.onTouchEvent(event);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-//        wordPaint.setColor(word_color);
-//        canvas.drawText(word_content,);
     }
 
     /**
