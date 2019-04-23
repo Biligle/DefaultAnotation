7ee043296f6aef6d5df9a3f177e5777e8b3b2b6b
wangguoli <wangguoli@bicai365.com>
2019-04-23 17:28:42 +0800

    上传gif图

--- app/src/main/java/com/biligle/defaultanotation/MainActivity.java
+++ app/src/main/java/com/biligle/defaultanotation/MainActivity.java
@@ -2,10 +2,6 @@ package com.biligle.defaultanotation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
