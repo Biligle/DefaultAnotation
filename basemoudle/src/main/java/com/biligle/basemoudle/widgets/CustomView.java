55a91bf2daa51cc54b1703b1eac7428aa5ef8d98
wangguoli <wangguoli@bicai365.com>
2019-06-11 16:54:55 +0800

    修改CustomView

--- basemoudle/src/main/java/com/biligle/basemoudle/widgets/CustomView.java
+++ basemoudle/src/main/java/com/biligle/basemoudle/widgets/CustomView.java
@@ -2,16 +2,21 @@ package com.biligle.basemoudle.widgets;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.appcompat.widget.AppCompatButton;
+
+import java.lang.reflect.Method;
 
 /**
  * @Author wangguoli
@@ -21,6 +26,7 @@ public class CustomView extends AppCompatButton {
     private int lastX, lastY;
     private int offsetX, offsetY;
     private int maxWidth, maxHeight;
+    private boolean mInPortrait;
 
     public CustomView(Context context) {
         super(context);
@@ -47,8 +53,13 @@ public class CustomView extends AppCompatButton {
         Display display = manager.getDefaultDisplay();
         display.getMetrics(dm);
         maxWidth = dm.widthPixels;
-        maxHeight = dm.heightPixels - getNavigationBarHeight() - getStatusHeight(context) - getTitleHeight((Activity) context) - getActionBatHeight(context);
+        int navigationBar = getNavigationBarHeight(context);
+        int status = getStatusHeight(context);
+        int title = getTitleHeight((Activity) context);
+        int actionBar = getActionBarHeight(context);
+        maxHeight = dm.heightPixels - status - title;
         /*---------------------------------------初始化高度结束——————————————————————————————————————*/
+        mInPortrait = (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
     }
 
     @Override
@@ -105,18 +116,83 @@ public class CustomView extends AppCompatButton {
     /**
      * 获取导航栏高度
      */
-    public int getNavigationBarHeight() {
-        Resources resources = getResources();
-        int resourceId = resources.getIdentifier("config_showNavigationBar", "bool", "android");
-        if (resourceId > 0) {
-            if (resources.getBoolean(resourceId)) {//导航栏是否显示
-                resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-                if (resourceId > 0) {
-                    return resources.getDimensionPixelSize(resourceId);//获取高度
+    public int getNavigationBarHeight(Context context) {
+        Resources res = context.getResources();
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context)) {
+                String key;
+                if (mInPortrait) {
+                    key = "navigation_bar_height";
+                } else {
+                    key = "navigation_bar_height_landscape";
                 }
+                return getInternalDimensionSize(res, key);
             }
         }
-        return 0;
+        return result;
+    }
+
+    /**
+     * 获取导航栏宽度
+     */
+    private int getNavigationBarWidth(Context context) {
+        Resources res = context.getResources();
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context)) {
+                return getInternalDimensionSize(res, "navigation_bar_width");
+            }
+        }
+        return result;
+    }
+
+    //通过此方法获取资源对应的像素值
+    private int getInternalDimensionSize(Resources res, String key) {
+        int result = 0;
+        int resourceId = res.getIdentifier(key, "dimen", "android");
+        if (resourceId > 0) {
+            result = res.getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    //通过此方法判断是否存在navigation bar
+    private boolean hasNavBar(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (resourceId != 0) {
+            boolean hasNav = res.getBoolean(resourceId);
+            // 查看是否有通过系统属性来控制navigation bar。
+            if ("1".equals(getNavBarOverride())) {
+                hasNav = false;
+            } else if ("0".equals(getNavBarOverride())) {
+                hasNav = true;
+            }
+            return hasNav;
+        } else {
+            //可通过此方法来查看设备是否存在物理按键(menu,back,home键)。
+            return !ViewConfiguration.get(context).hasPermanentMenuKey();
+        }
+    }
+
+    // 安卓系统允许修改系统的属性来控制navigation bar的显示和隐藏，此方法用来判断是否有修改过相关属性。
+    // (修改系统文件，在build.prop最后加入qemu.hw.mainkeys=1即可隐藏navigation bar)
+    // 相关属性模拟器中有使用。
+    // 当返回值等于"1"表示隐藏navigation bar，等于"0"表示显示navigation bar。
+    private String getNavBarOverride() {
+        String isNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                isNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+                isNavBarOverride = null;
+            }
+        }
+        return isNavBarOverride;
     }
 
     /**
@@ -152,12 +228,14 @@ public class CustomView extends AppCompatButton {
     /**
      * 获取ActionBar高度
      */
-    public static int getActionBatHeight(Context context) {
+    public static int getActionBarHeight(Context context) {
         TypedValue tv = new TypedValue();
         int actionBarHeight = 0;
         try {
-            if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
-                actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+                    actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
